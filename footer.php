d447e42e18ad68c0897d9fc61a265fe0e749ded4
Janos Pasztor <janos@pasztor.at>
2016-12-08 22:37:23 +0100

    Initial version

---
+++ footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Warning! This code was written to be broken ON PURPOSE! Every single piece of it is bad quality ON PURPOSE! DON'T
+ * EVER USE THIS FOR ANYTHING! PLEASE!
+ */
+?>
+		</div>
+		<script
+			src="https://code.jquery.com/jquery-2.2.4.min.js"
+			integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+			crossorigin="anonymous"></script>
+		<script
+			src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+			integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+			crossorigin="anonymous"></script>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Warning! This code was written to be broken ON PURPOSE! Every single piece of it is bad quality ON PURPOSE! DON'T
+ * EVER USE THIS FOR ANYTHING! PLEASE!
+ */
+?>
+<?php require(__DIR__ . '/header.php');?>
+<div class="row">
+	<div class="col-sm-push-3 col-sm-6">
+		<form action="login.php" method="post">
+			<h1>Please log in:</h1>
+			<div class="form-group">
+				<label for="username">Username</label>
+				<input type="text" class="form-control" id="username" placeholder="Username" autofocus="autofocus" />
+			</div>
+			<div class="form-group">
+				<label for="password">Password</label>
+				<input type="password" class="form-control" id="password" placeholder="Password" />
+			</div>
+			<button type="submit" class="btn btn-default">Submit</button>
+		</form>
+	</div>
+</div>
+<?php require(__DIR__ . '/footer.php');?>
